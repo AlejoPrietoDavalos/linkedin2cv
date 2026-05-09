3f7c88a7c0350c7fee1ee82eeb27674a76743f8e
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2026-05-09 20:26:12 -0300

    PATH_PDF_OUTPUT and compressed using ghostscript

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ load_dotenv()
 from src.app.drivers.build_cv.service import BuildCVService
 from src.app.drivers.font_loader import FontLoader, FontLoaderConfig
 from src.app.drivers.ghostscript import GhostScript
-from src.core.constants import PATH_DATA_DIR, PATH_FOLDER_DATA, PDF_EXTENSION
+from src.core.constants import PATH_PDF_OUTPUT
 from src.core.entities import PersonalInformation
 from src.app.drivers.linkedin_csv_repository import LinkedinCSVRepository
 
 
-def load_fonts() -> None:
+def _load_fonts() -> None:
     FONT_NAME = os.getenv("FONT_NAME")
     if not FONT_NAME:
         raise ValueError("FONT_NAME environment variable is required")
@@ -22,31 +22,29 @@ def load_fonts() -> None:
 
 
 def main(*, path_pdf: Path, personal_information: PersonalInformation) -> None:
-    load_fonts()
-
-    linkedin_data_repository = LinkedinCSVRepository()
-    linkedin_data = linkedin_data_repository.load_linkedin_data()
-
+    _load_fonts()
     builder_cv = BuildCVService()
     builder_cv.build_and_save(
         path_pdf=path_pdf,
         personal_information=personal_information,
         linkedin_data=linkedin_data,
     )
-    # FIXME: Desactivado temporalmente el dibujado de líneas extra sobre el PDF.
-    # builder_cv.draw_lines(path_pdf=path_pdf)
-
-    path_pdf_final = path_pdf.with_name(f'Curriculum - {linkedin_data.profile.full_name}.pdf')
-    shutil.copy(path_pdf, path_pdf_final)
-
-    # FIXME: Funciona pero no se por qué hay que hacerlo.
-    ghostscript = GhostScript()
-    ghostscript.compress_pdf(path_pdf_final)
 
 
 
 if __name__ == "__main__":
-    path_pdf = PATH_DATA_DIR / f"{PATH_FOLDER_DATA.stem}{PDF_EXTENSION}"
-
     personal_information = PersonalInformation()
-    main(path_pdf=path_pdf, personal_information=personal_information)
+
+    linkedin_data_repository = LinkedinCSVRepository()
+    linkedin_data = linkedin_data_repository.load_linkedin_data()
+
+    main(path_pdf=PATH_PDF_OUTPUT, personal_information=personal_information)
+
+    # FIXME: Desactivado temporalmente el dibujado de líneas extra sobre el PDF.
+    # builder_cv.draw_lines(path_pdf=path_pdf)
+    PATH_PDF_OUTPUT_COMPRESSED = PATH_PDF_OUTPUT.with_name(f'Curriculum - {linkedin_data.profile.full_name}.pdf')
+    shutil.copy(PATH_PDF_OUTPUT, PATH_PDF_OUTPUT_COMPRESSED)
+
+    # FIXME: Funciona pero no se por qué hay que hacerlo.
+    ghostscript = GhostScript()
+    ghostscript.compress_pdf(PATH_PDF_OUTPUT_COMPRESSED)
