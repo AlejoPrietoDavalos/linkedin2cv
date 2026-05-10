cc904c25f724381fe1006f9cba682d68b31f0f3d
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2026-05-09 23:45:31 -0300

    Change texts

--- src/core/hardcoded_config.py
+++ src/core/hardcoded_config.py
@@ -18,31 +18,38 @@ LABEL_WEBSITE_EN = " Inglés"
 LABEL_GITHUB = "➤➤ GitHub"
 LABEL_LINKEDIN = "➤➤ LinkedIn"
 
+
+
+
 SECTION_ABOUT_ME_TITLE = "Sobre mi"
 SECTION_ABOUT_ME_TEXT = (
-    "Programo soluciones end-to-end en <b>Python</b>, soy resolutivo y me motivan mucho los desafíos.<br/>"
+    "Programo soluciones end-to-end en Python, soy resolutivo y me motivan mucho los desafíos.<br/>"
     "Con gran interés en colaborar en proyectos de software/datos junto a otros profesionales."
 )
+
+
+
+
 SECTION_GOAL_TITLE = "Objetivo profesional"
 SECTION_GOAL_TEXT = (
-    "Poder aplicar <b>Python</b> en todo, siempre dispuesto a aprender nuevas tecnologías, "
-    "especialmente en <b>Ciencia de Datos</b>."
+    "Poder aplicar Python en todo, siempre dispuesto a aprender nuevas tecnologías, "
+    "especialmente en Ciencia de Datos."
 )
+
+
+
 SECTION_TECH_SUMMARY_TITLE = "Resumen técnico"
 SECTION_PROJECTS_TITLE = "Proyectos personales"
 SECTION_PROJECTS_TEXT = (
-    "● Tool para músicos usando Machine Learning.<br/>"
-    "➣ Descomposición de instrumentos en pistas.<br/>"
-    "➣ Cálculo de tempo, análisis de espectrograma.<br/><br/>"
     "● Teledetección de barcos para pesca ilegal.<br/>"
     "➣ Análisis de imágenes satelitales SAR.<br/>"
     "➣ Deep Learning para detección de objetos.<br/><br/>"
-    "● Chatbot de Whatsapp con IA para restaurant.<br/>"
-    "➣ El producto final tomará el pedido del usuario.<br/><br/>"
-    "● Automatizaciones para streaming.<br/>"
-    "➣ Desarrollé un juego en Python con interacción.<br/>"
-    "➣ Scripting para resolver tareas repetitivas.<br/>"
+    "● Sistema de procesamiento de datos geoespaciales.<br/>"
+    "➣ Resuelve problemáticas comunes para éste tipo de dato.<br/>"
 )
+
+
+
 
 JOB_SUBTITLE_PREFIX = "➤➤"
 LABEL_CURRENTLY = "Actualidad"
